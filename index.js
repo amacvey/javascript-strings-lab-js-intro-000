ccb8ce6c9e3ad8232dd82aae6f3da2098943c052
Andrew MacVey <macvey@usc.edu>
2019-06-21 19:24:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-'{greeting}  specialguest';
+'Hello,  {specialguest}!';
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
